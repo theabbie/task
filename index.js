9d7f0e77b73de7c468f72423f70fc132464f5ab8
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-09-30 16:19:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,175 +1,31 @@
-const puppeteer = require('puppeteer');
 const axios = require("axios");
 const fs = require("fs");
-var cheerio = require("cheerio");
-const util = require("util");
 var url = require("url");
 var devRant = require("rantscript");
-const save = util.promisify(fs.writeFile);
-var imgur = require("imgur");
-
-async function load(url) {  
-  const writer = fs.createWriteStream("meme.jpg")
-  const response = await axios({
-    url,
-    method: 'GET',
-    responseType: 'stream'
-  })
-  response.data.pipe(writer)
-  return new Promise((resolve, reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)
-  })
-}
 
 (async function() {
-  var browser = await puppeteer.launch({args: ['--no-sandbox']});
-  var page = await browser.newPage();
-  try {
-  await page.goto("https://9gag.com/shuffle");
-  var url = await page.evaluate(function() {
-  	return document.querySelector("link[rel='image_src']").href;
-  });
-  await page.setUserAgent('Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Mobile Safari/537.36');
-  await page.setCookie(...[
-  {
-    name: 'urlgen',
-    value: '"{\\"1.186.110.133\\": 45769}:1kNGGB:IRlL7EwssOy0oohHfhoOYlLe9rU"',
-    domain: '.instagram.com',
-    path: '/',
-    expires: -1,
-    size: 69,
-    httpOnly: true,
-    secure: true,
-    session: true
-  },
-  {
-    name: 'shbts',
-    value: '1601388336.701335',
-    domain: '.instagram.com',
-    path: '/',
-    expires: 1601993137.511523,
-    size: 22,
-    httpOnly: true,
-    secure: true,
-    session: false
-  },
-  {
-    name: 'sessionid',
-    value: '8047292047%3AcSFRI6LvAEq7Yd%3A3',
-    domain: '.instagram.com',
-    path: '/',
-    expires: 1632924336.540958,
-    size: 40,
-    httpOnly: true,
-    secure: true,
-    session: false
-  },
-  {
-    name: 'shbid',
-    value: '4688',
-    domain: '.instagram.com',
-    path: '/',
-    expires: 1601993137.511479,
-    size: 9,
-    httpOnly: true,
-    secure: true,
-    session: false
-  },
-  {
-    name: 'rur',
-    value: 'ASH',
-    domain: '.instagram.com',
-    path: '/',
-    expires: -1,
-    size: 6,
-    httpOnly: true,
-    secure: true,
-    session: true
-  },
-  {
-    name: 'csrftoken',
-    value: 'uEsM1nmPdRHPBWeIerjFm5nHeRajfPHT',
-    domain: '.instagram.com',
-    path: '/',
-    expires: 1632837939.678891,
-    size: 41,
-    httpOnly: false,
-    secure: true,
-    session: false
-  },
-  {
-    name: 'mid',
-    value: 'X3M_HQALAAHVbyLBr570SAMGRli6',
-    domain: '.instagram.com',
-    path: '/',
-    expires: 1916748317.516303,
-    size: 31,
-    httpOnly: false,
-    secure: true,
-    session: false
-  },
-  {
-    name: 'ds_user_id',
-    value: '8047292047',
-    domain: '.instagram.com',
-    path: '/',
-    expires: 1609164339.679007,
-    size: 20,
-    httpOnly: false,
-    secure: true,
-    session: false
-  },
-  {
-    name: 'ig_did',
-    value: 'BA1AAA04-C324-4D5D-9AF6-79ED8C88320E',
-    domain: '.instagram.com',
-    path: '/',
-    expires: 1916748317.159145,
-    size: 42,
-    httpOnly: true,
-    secure: true,
-    session: false
-  }
-]);
-  await load(url);
-  await page.setViewport({width: 330, height: 530});
-  await page.goto("https://instagram.com/",{waitUntil: 'networkidle0'});
+   try {
+     var rants = [];
+     var maps = ["https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap.xml","https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap_2.xml","https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap_3.xml","https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap_4.xml","https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap_5.xml"]
 
- const [fileChooser] = await Promise.all([
-  page.waitForFileChooser(),
-  page.click("div[data-testid='new-post-button']")
-]);
-
-await fileChooser.accept(['meme.jpg']);
-
-await page.waitFor(1000);
-
-await page.evaluate(function() {
-  document.querySelectorAll("button")[1].click();
-});
-
-await page.waitFor(500);
-
-await page.evaluate(function() {
-  document.querySelectorAll("textarea")[0].value = "Test Image";
-});
-
-await page.evaluate(function() {
-  document.querySelectorAll("button")[1].click();
-});
-  
-await page.waitFor(2000);
-    
-  await page.screenshot({path: 'ss.png'});
-  var link = (await imgur.uploadFile('ss.png')).data.link;
-  console.log(link);
-  }
-  catch (e) {
-  console.log(e.message);
-  await page.screenshot({path: 'ss.png'});
-  var link = (await imgur.uploadFile('ss.png')).data.link;
-  console.log(link);
-  }
-  await browser.close();
+     for (p of maps) {
+       var k = cheerio.load((await axios(p,{headers: {"User-Agent": "Googlebot-News"}})).data,{xmlMode: true})
+       rants = [...rants, ...k("loc").map((i,x)=>+url.parse(k(x).text(),true).pathname.split("/")[2]).get()]
+     }
+     var scores = {};
+     for (id of rants) {
+         try {
+         var rant = await devRant.rant(id);
+         scores[rant.rant["user_id"]] = rant.rant["user_score"];
+         for (comment of rant.comments) {
+           scores[comment["user_id"]] = comment["user_score"];
+       }
+      }
+       catch (e) {continue;}
+     }
+   
+ }
+ catch (e) {
+   console.log(e.message);
+ }
 })();
