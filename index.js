aba4feb576431968785682a06b398c4e2ddb4c7d
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-10-05 22:17:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,42 +5,19 @@ var devRant = require("rantscript");
 var cheerio = require("cheerio");
 
 (async function() {
-   var scores = {};
    try {
-     /*var i = 215000;
-     var rants = (await axios("https://del.dog/raw/rants")).data.split("\n").slice(215000);
+     var rants = (await axios("https://del.dog/raw/rants")).data.split("\n").slice(0,1000);
      console.log("START");
      for (id of rants) {
-         i++;
          try {
-         if (i%1000==0) {console.log(i);await axios.post("https://admin-2s999zpqryz3.runkit.sh/",scores);}
          var rant = await devRant.rant(id);
-         scores[rant.rant["user_id"]] = rant.rant["user_score"];
-         for (comment of rant.comments) {
-           scores[comment["user_id"]] = comment["user_score"];
-       }
+         var time = new Date(rant.rant["created_time"]*1000);
+         await axios.post("https://admin-2s999zpqryz3.runkit.sh/?time=" + d.getFullYear() + '/' + d.getMonth() +'/' + d.getDate() + '/' + d.getDay());
       }
        catch (e) {continue;}
-     }*/
-     scores = (await axios("https://codrcrew.firebaseio.com/main/devrant.json")).data;
-     var ranks = [];
-     for (x in scores) {
-       ranks.push([x,scores[x]]);
-    }
-    ranks = ranks.sort((a,b)=>b[1]-a[1]).slice(0,1000);
-    var nr = [];
-    for (t of ranks) {
-        var profile = (await devRant.profile(t[0]));
-        nr.push([profile.username,[...profile.content.content.rants.map(x=>x["num_comments"]),0].reduce((a,b)=>a+b)]);
-    }
-    nr = nr.sort((a,b)=>b[1]-a[1]);
-    var k = 0;
-    for (t of nr) {
-        console.log(++k+". @"+t[0]+": "+t[1]);
      }
  }
  catch (e) {
    console.log(e.message);
-   console.log(scores);
  }
 })();
