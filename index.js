43292480b2e7d8ffaf4f075fd88d42280027a096
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-11-16 20:41:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const fs = require("fs");
 var url = require("url");
 var devRant = require("rantscript");
 var cheerio = require("cheerio");
+var i = 0;
 
 (async function() {
    try {
      console.log("START");
      while (true) {
        try {
+         console.log(i++);
          await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713208");
          await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=714267");
          await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713412");
