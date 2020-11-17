dfdbb4a1bf494d066359b691ce593a5daf9cefef
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-11-17 14:51:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,8 @@ var i = 0;
      while (true) {
        try {
          console.log(i++);
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713208");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=714267");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713412");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713506");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713258");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713317");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=711527");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=711520");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713393");
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=713425");
+         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=715421");
+         await axios("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_like&post_id=715421");
        }
        catch (e) {continue;}
    }
