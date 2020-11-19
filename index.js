c5049cd6a8d9c0f27509b7aa928d8cb7ccc2201a
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-11-19 16:35:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const axios = require("axios");
-const fs = require("fs");
-var url = require("url");
-var devRant = require("rantscript");
-var cheerio = require("cheerio");
-var i = 0;
+var Ifunny = require("node-ifunny");
+var rgag = require("random-gag");
 
 (async function() {
-   try {
-     console.log("START");
-     while (true) {
-       try {
-         console.log(i++);
-         await axios.post("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_view_count&post_id=714624");
-         await axios("https://www.youthkiawaaz.com/wp-admin/admin-ajax.php?action=yka_like&post_id=714624");
-       }
-       catch (e) {continue;}
-   }
- }
- catch (e) {
-   console.log(e.message);
- }
+ try {
+  var ifunny = new Ifunny();
+  await ifunny.addToken(process.argv[2]);
+  var gag = await rgag();
+  //while (gag.type!="Photo") gag = await rgag();
+  if (gag.type=="Photo") res.json(await ifunny.postimg(gag.images.image700.url,gag.title,gag.tags.map(x=>x.key)));
+  else res.json(await ifunny.postvid(gag.images.image460sv.url,gag.title,gag.tags.map(x=>x.key)));
+  }
+  catch (e) {
+    res.end(e.message);
+  }
 })();
