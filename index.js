61569b825644be2fa46ec871cbcb896ad3db44b6
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-11-19 16:37:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var rgag = require("random-gag");
   await ifunny.addToken(process.argv[2]);
   var gag = await rgag();
   //while (gag.type!="Photo") gag = await rgag();
-  if (gag.type=="Photo") res.json(await ifunny.postimg(gag.images.image700.url,gag.title,gag.tags.map(x=>x.key)));
-  else res.json(await ifunny.postvid(gag.images.image460sv.url,gag.title,gag.tags.map(x=>x.key)));
+  if (gag.type=="Photo") await ifunny.postimg(gag.images.image700.url,gag.title,gag.tags.map(x=>x.key));
+  else await ifunny.postvid(gag.images.image460sv.url,gag.title,gag.tags.map(x=>x.key));
   }
   catch (e) {
     res.end(e.message);
