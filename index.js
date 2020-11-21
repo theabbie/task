c57cd98e60051498073624ec92d73996305d2052
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-11-21 10:38:06 +0530

    [skip ci]

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var Ifunny = require("node-ifunny");
 var rgag = require("random-gag");
+var uns = require("unshorten.it");
 
 (async function() {
  try {
-  var ifunny = new Ifunny();
-  await ifunny.addToken(process.argv[2]);
    while (true) {
     try {
-      var gag = await rgag();
-      //while (gag.type!="Photo") gag = await rgag();
-      if (gag.type=="Photo") console.log(await ifunny.postimg(gag.images.image700.url,gag.title,gag.tags.map(x=>x.key)));
-      else console.log(await ifunny.postvid(gag.images.image460sv.url,gag.title,gag.tags.map(x=>x.key)));
+      await uns("https://g.co/kgs/x7WjhR");
      }
      catch (e) {
       console.log(e.message);
